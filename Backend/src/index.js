eccd86142212399be4f76cbe0b4156b7ffb506a5
abuki43 <120245789+abuki43@users.noreply.github.com>
2025-05-17 02:52:12 -1200

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,49 +16,6 @@ app.use(
   );
 const PORT = process.env.PORT || 3000;
 
-
-
-
-// app.post('/api/auth/sign-in/email' , async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-
-//         if (!email || !password) {
-//             return res.status(400).json({ message: "Data not found!" });
-//         }
-
-//         console.log(`Request sent from email: ${email} with password: ${password}`);
-
-//         const response = await auth.api.signInEmail({
-//             body: { email, password },
-//             asResponse: true
-//         });
-
-//         console.log("Response", response);
-//         res.status(200).json({ message: "SUCCESSFULLY SIGNED IN" });
-//     } catch (error) {
-//         console.error("Error:", error);
-//         res.status(400).json({ message: "Invalid JSON" });
-//     }
-// });
-
-// app.post("/api/auth/sign-up/email", async(req,res)=>{
-//     const { email, password,name } = req.body;
-
-//         if (!email || !password) {
-//             return res.status(400).json({ message: "Data not found!" });
-//         }
-
-//         console.log(`Request sent from email: ${email} with password: ${password} : name ${name}`);
-
-//         const response = await auth.api.signUpEmail({
-//             body: { email, password,name },
-//             asResponse: true
-//         });
-
-//         console.log("Response:", response);
-//         res.status(200).json({ message: "SUCCESSFULLY SIGNED IN" });
-// })
 app.all("/api/auth/*", toNodeHandler(auth));
 
 app.use(express.json()); 
